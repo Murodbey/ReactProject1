4ed129546c39c3d3cbd3f0b948f7f664f9a8a0ce
Murodbey <abdullayevmurod@gmail.com>
2024-02-20 18:07:50 -0600

    Second Component Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import FirstComponent from './components/FirstComponent'
+import SecondComponent from './components/SecondComponent';
 
 function App() { //Component example   JSX
   const name = 'Murod'
   return (
     <div className="App">
       Hello World, My name is {name}
-      <FirstComponent/>
+      <FirstComponent />
+      <SecondComponent />
     </div>
   );
 }
---
+++ src/components/SecondComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SecondComponent = () => {
+    return (
+        < button >
+            Click Me
+        </button >
+    )
+}
+
+export default SecondComponent;
